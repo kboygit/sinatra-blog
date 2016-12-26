14b529c7bb18377459edf1030ddb772505034e35
kboygit <heybisayangdako@gmail.com>
2016-12-25 16:34:28 -0800

    commented out simplify main,render posts

--- main.rb
+++ main.rb
@@ -1,19 +1,24 @@
 require "sinatra"
 require 'sinatra/reloader'
 require "haml"
+require './model/blog.rb'
+
+db = Blog.new()
 
 get '/' do
-  # "Hello World! <a href='/another'> click me </a>"
+  # # "Hello World! <a href='/another'> click me </a>"
+  # haml :index
+  @posts = db.posts()
   haml :index
 end
 
-get '/another' do
-  # "Hello Stranger"
-  haml :another
-end
-
-get '/name/:  name' do
-  @name = params[:name]
-  haml :name
-  # "Your name is #{params[:name]}"
-end
+# get '/another' do
+#   # "Hello Stranger"
+#   haml :another
+# end
+#
+# get '/name/:  name' do
+#   @name = params[:name]
+#   haml :name
+#   # "Your name is #{params[:name]}"
+# end
